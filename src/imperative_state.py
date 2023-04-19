bdd3c25f97e5da96a30d550efd4838c7f361cc48
honeykjoule <honeykjoule@proton.me>
2023-04-19 04:11:24 +0000

    42 lines simple pilled

--- src/imperative_state.py
+++ src/imperative_state.py
@@ -4,15 +4,11 @@ from dotenv import load_dotenv
 import openai
 
 load_dotenv()
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def get_chat_completion(messages, prompt):
     messages.append({"role": "user", "content": prompt})
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
     return response
 
 def update_state(messages, objective):
@@ -28,33 +24,19 @@ def update_imperative(messages, objective):
     return new_imperative.choices[0].message
 
 if __name__ == "__main__":
-    
+   
     objective = "Grow a company to sell legal personality to artificial intelligence."
-
-    messages = [
-        {"role": "system", "content": "You are a simple-pilled, greetext poster. Your instructions are to maintain and update the state and imperative for the user's project. The state represents the memory for the project. The imperative represents the next task to complete. Return all responses in greentext format."},
-    ]
-
-    print("initialized")
+    messages = [{"role": "system", "content": "You are a simple-pilled, greetext poster. Your instructions are to maintain and update the state and imperative for the user's project. The state represents the memory for the project. The imperative represents the next task to complete. Return all responses in greentext format."},]
+   
     max_iterations = 2
     iteration_counter = 0
-    
+   
     while iteration_counter < max_iterations:
-        
-        print("calling update state")
         state = update_state(messages, objective)
-        print("Updated state:", messages[-1]["content"])
-        
-        print("calling update imperative")
         imperative = update_imperative(messages, objective)
-        print("Updated imperative:", messages[-1]["content"])
-
         iteration_counter += 1
-        print(f"Iteration {iteration_counter}:")
-        
     
     output_path = os.getenv("DATA_PATH").encode('utf-8')
     filename = os.path.join(output_path, b"messages.json")
-
     with open(filename, 'w') as f:
         json.dump(messages, f, indent=4)
